be86b946d19f51c3ac1508f3b0acfa027735f231
vivekgit-20 <viveksharma20092004@gmail.com>
2026-06-10 18:57:10 +0530

    Update CORS origin for production environment

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interviewiq-vaph.onrender.com",
     credentials:true
 }))
 
