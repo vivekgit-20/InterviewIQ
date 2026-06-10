50cf891819d134ece74f9854a289488c5e4d7b4c
vivekgit-20 <viveksharma20092004@gmail.com>
2026-06-10 18:02:04 +0530

    Update ServerUrl to production backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "https://interviewiq-backend-002j.onrender.com"
 
 function App() {
 
